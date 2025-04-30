5257f63f19b9e2a1f27718ebe0a81cbeea0f4724
Akhil Jain <akhiljda@gmail.com>
2025-04-30 23:08:34 +0530

    changes

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
-    headless: false, 
+    headless: true, 
     //navigationTimeout: 40000,
     //actionTimeout: 30000,
     video: 'on',
